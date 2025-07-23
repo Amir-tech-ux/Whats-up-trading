da541559b0559b91f5b1324d09ae7c52fff8e722
Amir-tech-ux <amirozany@gmail.com>
2025-07-23 08:50:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-from flask import Flask, request, jsonify
 
-app = Flask(__name__)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    print("Incoming data:", data)
-
-    reply = {
-        "text": "התקבל! ✅ הבוט פעיל!"
-    }
-
-    return jsonify(reply)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
