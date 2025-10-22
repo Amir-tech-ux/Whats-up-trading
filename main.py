8f3ac91155b5f50ba86fb7511d8523ba7894dfc2
Amir-tech-ux <amirozany@gmail.com>
2025-10-22 03:54:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os, json, requests
+if __name__ == "__main__":
+    from waitress import serve
+    port = int(os.environ.get("PORT", 10000))
+    serve(app, host="0.0.0.0", port=port)import os, json, requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
