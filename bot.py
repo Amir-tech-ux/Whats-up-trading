60849c06b301c4e23f229de9dd9df7638d5a9a98
Amir-tech-ux <amirozany@gmail.com>
2025-12-07 04:39:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,20 @@ import os
 from flask import Flask, request
 import telebot
 
-TOKEN = "YOUR_TOKEN"
+TOKEN = "הכנס_כאן_את_הטוקן_שלך"
 bot = telebot.TeleBot(TOKEN)
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "OK", 200
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return "OK", 200
+    else:
+        return "Unsupported Media Type", 415
 
 
 @bot.message_handler(commands=['ping'])
@@ -27,7 +30,5 @@ def echo_handler(message):
 
 
 if __name__ == '__main__':
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
